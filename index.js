4a04bccad832b328ac84a0fbe244057cecb37275
GiovannaBalotin <giovannabalotin@gmail.com>
2023-11-27 16:06:17 -0300

    desafio atualizado

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
-let nome = "Giovanna";
-console.log(nome);
+// Definindo variáveis
+var nomeDoHeroi = "Giovanna"; 
+var xpDoHeroi = 10000; 
+
+// Utilizando estrutura de decisão para determinar a categoria
+var categoria;
+if (xpDoHeroi < 1000) {
+    categoria = 
+    categoria =
+"Ferro";
+} 
+
+else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    categoria = 
+   
+"Bronze";
+} 
+
+else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    categoria = 
+    categoria =
+
+    categoria
+"Prata";
+} 
+
+else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
+    categoria = 
+   
+"Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    categoria = 
+    categoria =
+
+    categoria
+"Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    categoria = 
+    categoria
+"Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    categoria = 
+    categoria =
+
+    categoria
+
+   
+"Imortal";
+} 
+
+else {
+    categoria = 
+    categoria =
+
+    categoria
+
+   
+"Radiante";
+}
+
+// Exibindo a mensagem
+console.log("O herói " + nomeDoHeroi + " possui " + xpDoHeroi + " de XP e é da categoria " + categoria + ".");
